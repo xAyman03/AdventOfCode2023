7a6eb5ef982d9d197b9ecea02a7a55f2d4fafc0d
xAyman03 <111664924+xAyman03@users.noreply.github.com>
2023-12-06 07:48:40 +0100

    Add files via upload

---
+++ Day01/tmp.py
@@ -0,0 +1,29 @@
+document = []
+with open('t.txt') as f:
+    document = f.readlines()
+
+calibration = []
+
+sum = 0
+
+for t in document:
+    num1 = 0
+    num2 = 0
+    for i in range(len(t)):
+        if t[i].isdigit():
+            num1 = t[i]
+            break
+    for i in range(len(t) - 1, 0, -1):
+        if t[i].isdigit():
+            num2 = t[i]
+            break
+        else:
+            num2 = num1    
+                
+    calibration.append(num1 + num2)
+    
+
+for i in calibration:
+    sum = sum + int(i)
+    
+print(sum)
